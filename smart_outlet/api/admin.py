f8e9fe68833c974eb8cdcf8184a43cd3c5a8605e
Joan Mwihava <joanmwihava08@gmail.com>
2026-05-17 12:05:06 +0300

    Show device IDs in admin panel

--- smart_outlet/api/admin.py
+++ smart_outlet/api/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import User, Device, EnergyRecord, ControlLog, SafetyAlert, ApplianceSchedule
 
-# Register all models to make them visible in the Django admin panel
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'device_name', 'location', 'status', 'user']
+
+@admin.register(EnergyRecord)
+class EnergyRecordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'device', 'voltage', 'current', 'power', 'timestamp']
+
+@admin.register(SafetyAlert)
+class SafetyAlertAdmin(admin.ModelAdmin):
+    list_display = ['id', 'device', 'alert_type', 'measured_value', 'timestamp']
+
+@admin.register(ControlLog)
+class ControlLogAdmin(admin.ModelAdmin):
+    list_display = ['id', 'device', 'action', 'control_source', 'timestamp']
+
 admin.site.register(User)
-admin.site.register(Device)
-admin.site.register(EnergyRecord)
-admin.site.register(ControlLog)
-admin.site.register(SafetyAlert)
 admin.site.register(ApplianceSchedule)
